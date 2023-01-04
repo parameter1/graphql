691a71a367e295bfae0ef771c0ea0ea2590c91b9
Jacob Bare <jacob@parameter1.com>
2023-01-04 15:46:23 -0500

    Create `graphql-apollo-plugin-operation-profiler` package

---
+++ packages/apollo/plugins/operation-profiler/src/index.js
@@ -0,0 +1,36 @@
+import { isIntrospectionQuery } from '@parameter1/graphql-utils';
+import { get } from '@parameter1/object-path';
+
+const { log } = console;
+
+const setHeader = (http, key, value) => {
+  if (http) http.headers.set(key, `${value}`);
+};
+
+export default function operationProfilerPlugin({ enabled = true, logToTerminal = true } = {}) {
+  const shouldProfile = (requestContext) => {
+    if (!enabled) return false;
+    return !isIntrospectionQuery(requestContext);
+  };
+  return {
+    /**
+     *
+     */
+    requestDidStart(rq) {
+      let start;
+      if (shouldProfile(rq)) start = process.hrtime();
+      return {
+        willSendResponse(requestContext) {
+          if (!shouldProfile(requestContext) || !start) return;
+          const { operationName: name, response } = requestContext;
+          const { http } = response;
+          const [secs, ns] = process.hrtime(start);
+          const ms = (secs * 1000) + (ns / 1000000);
+          const type = get(requestContext, 'operation.operation');
+          if (logToTerminal) log('Apollo Op Profile', { type, name, ms });
+          setHeader(http, 'x-profile-time-ms', ms);
+        },
+      };
+    },
+  };
+}
